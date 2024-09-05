0110bb84e5a4d39541b6c573d41d8742eba851eb
Federico Coscia <federico.coscia1@studenti.unimi.it>
2024-09-05 12:40:00 +0200

    redesigned main window construction

--- backend/WebcamRecorder.py
+++ backend/WebcamRecorder.py
@@ -2,11 +2,18 @@ import threading
 import cv2
 import queue
 
+class WebcamNotFoundError(Exception):
+    pass
+
 class WebcamRecorder(threading.Thread):
     
     def __init__(self, output_file, daemon):
         super(WebcamRecorder, self).__init__()
+        
         self.cap = cv2.VideoCapture(0)
+        if not self.cap.isOpened():
+            raise WebcamNotFoundError
+        
         self.frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         self.fps = self.cap.get(cv2.CAP_PROP_FPS)
