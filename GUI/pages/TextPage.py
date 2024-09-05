fa23e31126eafecd28e5f68a0b4e05c7e04cb884
Federico Coscia <federico.coscia1@studenti.unimi.it>
2024-09-05 12:01:51 +0200

    improved file saving logic

--- GUI/pages/TextPage.py
+++ GUI/pages/TextPage.py
@@ -4,12 +4,13 @@ from PyQt5.uic import loadUi
 
 class TextPage(QWidget):
     
-    def __init__(self, parent, title, text, button_text="Avanti"):
+    def __init__(self, parent, title, text, button_text="Avanti", button_slot=None):
         super().__init__()
         self.parent_window = parent
         self.title_str = title
         self.text_str = text
         self.button_text = button_text
+        self.button_slot = button_slot
         
         loadUi("GUI/qtdesigner/TextPage.ui", self)
         self.setContentsMargins(500,300,500,300)
@@ -17,7 +18,11 @@ class TextPage(QWidget):
         self.title.setText(self.title_str)
         self.text.setText(self.text_str)
         self.ready_button.setText(self.button_text)
-        self.ready_button.clicked.connect(self.ready_button_clicked)
+        
+        if self.button_slot is not None:
+            self.ready_button.clicked.connect(self.button_slot)
+        else:
+            self.ready_button.clicked.connect(self.ready_button_clicked)
         
     def ready_button_clicked(self):
         self.parent_window.next_page()
