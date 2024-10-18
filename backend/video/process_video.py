c10b87f28d34deb7cd55f2af0a0af4110f77cc75
Federico Coscia <federico.coscia1@studenti.unimi.it>
2024-10-18 16:35:39 +0200

    moved tmp video files to subject dir

--- backend/video/process_video.py
+++ backend/video/process_video.py
@@ -2,17 +2,18 @@ import libreface
 import shutil
 import os
 
-this_dir = os.path.join(os.getcwd(), 'backend', 'datasync')
-temp_dir = os.path.join(this_dir, 'tmp')
+this_dir = os.path.join(os.getcwd(), 'backend', 'video')
 
 def process_video(subject_dir, device='cuda:0', video_file_name="recording.mp4", output_save_path="face_data.csv"):
+    
+    temp_dir = os.path.join(subject_dir, 'tmp')
     
     video_path = os.path.join(subject_dir, video_file_name)
     libreface.get_facial_attributes(video_path,
                                     device=device,
                                     output_save_path=output_save_path,
-                                    temp_dir= temp_dir,
-                                    weights_download_dir = this_dir)
+                                    temp_dir=temp_dir,
+                                    weights_download_dir =this_dir)
     
     shutil.rmtree(temp_dir, ignore_errors=True)
     
