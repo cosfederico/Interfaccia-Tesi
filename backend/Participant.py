fc3689d95d515a0585b8f6c3c2e0e8adf11944db
Federico Coscia <federico.coscia1@studenti.unimi.it>
2024-11-08 11:10:26 +0100

    renamed subject class to participant class

--- backend/Participant.py
+++ backend/Participant.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone
 import os
 import csv
 
-class Subject:
+class Participant:
     
     sep = ';'
     
@@ -18,15 +18,18 @@ class Subject:
         self.session_start_timestamp = None
         self.session_end_timestamp = None
         
-        self.video_start_timestamps = []
-        self.video_end_timestamps = []
-        self.video_answers = []
-        self.video_names = []
+        self.video_name = None
+        self.video_type = None
+        self.video_start_timestamp = None
+        self.video_end_timestamp = None
+        
+        self.questions = []
+        self.answers = []
         
     def timestamp(self):
         return datetime.now(timezone.utc).timestamp()
     
-    def subject_dir(self):
+    def participant_dir(self):
         return os.path.join(self.data_dir, str(self.id))
         
     def set_data(self, age:int, gender:str, nationality:str, english_level:str):
@@ -41,41 +44,34 @@ class Subject:
     def set_session_end_timestamp(self):
         self.session_end_timestamp =self.timestamp()
         
-    def add_video_start_timestamp(self, video_name:str):
-        self.video_names.append(video_name)
-        self.video_start_timestamps.append(self.timestamp())
+    def set_video_start_timestamp(self, video_name:str, video_type:str):
+        self.video_name = video_name
+        self.video_type = video_type
+        self.video_start_timestamp = self.timestamp()
         
-    def add_video_end_timestamp(self):
-        self.video_end_timestamps.append(self.timestamp())   
+    def set_video_end_timestamp(self):
+        self.video_end_timestamp = self.timestamp()
         
-    def add_video_answers(self, answers:list):
-        self.video_answers.append(answers)
+    def add_answer(self, question:str, answer:str):
+        self.questions.append(question.replace('\n', '').replace('\t',''))
+        self.answers.append(answer.replace('\n', '').replace('\t',''))
         
-    def dump_to_file(self, dest_dir:str):
+    def dump_to_file(self, dest_dir:str, filename='data.csv'):
         
-        with open(os.path.join(dest_dir, 'data.csv'), 'w', newline='') as file:
-            writer = csv.writer(file, delimiter=Subject.sep)
+        with open(os.path.join(dest_dir, filename), 'w', newline='') as file:
+            writer = csv.writer(file, delimiter=Participant.sep)
             
             date = datetime.today().strftime('%Y-%m-%d')
             
             fields = ["id", "age", "gender", "nationality", "english_level", "date", "session_start_timestamp", "session_end_timestamp"]
             row = [self.id, self.age, self.gender, self.nationality, self.english_level, date, self.session_start_timestamp, self.session_end_timestamp]
             
-            for i in range(len(self.video_start_timestamps)):
+            fields += ["video_name", "video_type", "video_start_timestamp", "video_end_timestamp"]
+            row += [self.video_name, self.video_type, self.video_start_timestamp, self.video_end_timestamp]
                 
-                fields.append('video' + str(i) + '_name')
-                fields.append('video' + str(i) + '_start_timestamp')
-                fields.append('video' + str(i) + '_end_timestamp')
-            
-                row.append(self.video_names[i])
-                row.append(self.video_start_timestamps[i])
-                row.append(self.video_end_timestamps[i])
-                
-                for j, answer in enumerate(self.video_answers[i]):
-                    fields.append('video' + str(i) + '_answer' + str(j))
-                    fields.append('video' + str(i) + '_answer' + str(j) + '_timestamp')
-                    row.append(answer[0].replace("\n"," ").replace("\t"," "))
-                    row.append(answer[1])
+            for question, answer in zip(self.questions, self.answers):
+                fields.append(question)
+                row.append(answer)
                 
             writer.writerow(fields)
             writer.writerow(row)
