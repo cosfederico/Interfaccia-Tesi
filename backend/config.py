4d70f8f86be6605ea686b2c46fb159ea82122ad6
Federico Coscia <federico.coscia1@studenti.unimi.it>
2024-11-24 17:15:13 +0100

    removed unnecessary checks in config file loading

--- backend/config.py
+++ backend/config.py
@@ -28,12 +28,6 @@ def load_config(config_file='config.json', schema_file="backend" + os.sep + "sch
     VIDEO_FOLDER = config['app']['VIDEO_FOLDER']
     PARTICIPANT_ID = config['empatica']['PARTICIPANT_ID']
 
-    if not os.path.isdir(DATA_FOLDER):
-        raise NotADirectoryError("The specified data folder in config.json is not a valid directory.")
-
-    if not os.path.isdir(VIDEO_FOLDER):
-        raise NotADirectoryError("The specified video folder in config.json is not a valid directory.")
-
     if len(PARTICIPANT_ID) > 10 or not (PARTICIPANT_ID.isupper() or PARTICIPANT_ID.isdigit()):
         raise ValueError("A Participant ID can have up to 10 characters and can only include upper-case letters and numbers. Found: \"" + PARTICIPANT_ID + "\"")
 
