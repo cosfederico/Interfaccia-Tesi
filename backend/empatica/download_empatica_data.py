f43d8294283ce8244e8b5edd828322bd7947641b
Federico Coscia <federico.coscia1@studenti.unimi.it>
2025-02-01 11:19:26 +0100

    added download of multiple avro files if data was split and added script for merging avro files

--- backend/empatica/download_empatica_data.py
+++ backend/empatica/download_empatica_data.py
@@ -2,19 +2,58 @@ import boto3
 import os
 import numpy as np
 
-def download_empatica_data(start_ts, subject_dir, date, participant, bucket_name, prefix, org_id, study_id):
+import avro.schema
+import avro.datafile
+import avro.io
+
+def concat_avro_files(input_files, output_file, delete_original_files=False):
+    
+    with open(input_files[0], "rb") as f:
+        reader = avro.datafile.DataFileReader(f, avro.io.DatumReader())
+        schema_json = reader.meta.get("avro.schema").decode("utf-8")
+        schema = avro.schema.parse(schema_json)
+    
+    with open(output_file, "wb") as out_f:
+        writer = avro.datafile.DataFileWriter(out_f, avro.io.DatumWriter(), schema)
+        
+        for file in input_files:
+            with open(file, "rb") as f:
+                reader = avro.datafile.DataFileReader(f, avro.io.DatumReader())
+
+                file_schema_json = reader.meta.get("avro.schema").decode("utf-8")
+                if file_schema_json != schema_json:
+                    raise ValueError(f"Schema mismatch in file: {file}")
+
+                for record in reader:
+                    writer.append(record)
+
+                reader.close()
+
+        writer.close()
+
+        if delete_original_files:
+            for file in input_files:
+                try:
+                    os.remove(file)
+                except:
+                    pass
+
+        return output_file
+
+def download_empatica_data(start_ts, end_ts, subject_dir, date, participant, bucket_name, prefix, org_id, study_id):
     
     s3_resource = boto3.resource('s3')
     bucket = s3_resource.Bucket(bucket_name)
     
     filter = prefix + org_id + "/" + study_id + "/participant_data/"+ date + "/" + participant
 
-    timestamps = []
-    keys = []
-    
     bucket_objects = bucket.objects.filter(Prefix = filter)
     if len(list(bucket_objects)) == 0:
         raise ValueError("No data found for the selected date: " + date + " and participant: " + participant)
+
+    timestamps = []
+    keys = []
+    keys_after = []
 
     for my_bucket_object in bucket_objects:
         
@@ -29,16 +68,28 @@ def download_empatica_data(start_ts, subject_dir, date, participant, bucket_name
         if avro_ts <= start_ts:
             timestamps.append(avro_ts)
             keys.append(key)
-      
+
+        if avro_ts > start_ts and avro_ts <= end_ts:
+            keys_after.append(key)
+        
     if len(timestamps) == 0:
         raise ValueError("No recorded data found for the time of this session")
   
-    selected_files = np.rec.fromarrays((timestamps, keys), names=('ts', 'keys'))    
-    file_to_download = selected_files.keys[np.argmax(selected_files.ts)]
-    filename = os.path.split(file_to_download)[1]
+    start_files = np.rec.fromarrays((timestamps, keys), names=('ts', 'keys'))    
+    file_to_download = start_files.keys[np.argmax(start_files.ts)]
 
-    downloaded_file = os.path.join(subject_dir, filename)
+    files_to_download = [file_to_download]
+    for key_after in keys_after:
+        files_to_download.append(key_after)
 
-    bucket.download_file(file_to_download, downloaded_file)
-    
-    return downloaded_file
+    downloaded_files = []
+    for file_to_download in files_to_download:
+        filename = os.path.split(file_to_download)[1]
+        downloaded_file = os.path.join(subject_dir, filename)
+        bucket.download_file(file_to_download, downloaded_file)
+        downloaded_files.append(downloaded_file)
+
+    if len(downloaded_files) == 1:
+        return downloaded_files[0]
+    else:
+        return concat_avro_files(downloaded_files, os.path.join(subject_dir, "empatica.avro"), delete_original_files=True)
