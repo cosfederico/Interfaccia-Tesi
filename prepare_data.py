8e0c97f8c1e5b91c547613d2850809e3766c78eb
Federico Coscia <federico.coscia1@studenti.unimi.it>
2025-02-04 10:34:40 +0100

    added script for automatic data processing (download, extraction, splitting into groups)

---
+++ prepare_data.py
@@ -0,0 +1,8 @@
+print("Downloading Empatica Data")
+import backend.data_processing.download_empatica_data
+print("Extracting Raw Gaze Data")
+import backend.data_processing.extract_raw_gaze_data
+print("Extracting Gaze Features")
+import backend.data_processing.extract_gaze_features
+print("Splitting Data")
+import backend.data_processing.split_data
