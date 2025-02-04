99acc644f31dba2d4ffbc2229a350bc6fc833b46
Federico Coscia <federico.coscia1@studenti.unimi.it>
2025-02-04 10:27:35 +0100

    reduced processing to 1 frame per second for speed purposes

--- backend/video/process_video.py
+++ backend/video/process_video.py
@@ -1,10 +1,10 @@
 from feat import Detector
 import os
 
-def process_video(participant_dir, device='cpu', video_file_name="webcam.mp4", output_file_name="face.csv"):
+def process_video(participant_dir, device='auto', video_file_name="webcam.mp4", output_file_name="face.csv"):
         
     video_path = os.path.join(participant_dir, video_file_name)
     
     detector = Detector(device=device)
-    detections = detector.detect_video(video_path)
+    detections = detector.detect_video(video_path, skip_frames=30, face_detection_threshold=0.9)
     detections.to_csv(os.path.join(participant_dir, output_file_name))
