db1d78a524ad2dc1997e2a5ab4a029b0b4f143cf
Federico Coscia <federico.coscia1@studenti.unimi.it>
2025-02-04 10:44:06 +0100

    added option for deleting video after processing

--- backend/video/process_video.py
+++ backend/video/process_video.py
@@ -1,10 +1,16 @@
 from feat import Detector
 import os
 
-def process_video(participant_dir, device='auto', video_file_name="webcam.mp4", output_file_name="face.csv"):
+def process_video(participant_dir, device='auto', video_file_name="webcam.mp4", output_file_name="face.csv", delete_video_after=False):
         
     video_path = os.path.join(participant_dir, video_file_name)
     
     detector = Detector(device=device)
     detections = detector.detect_video(video_path, skip_frames=30, face_detection_threshold=0.9)
     detections.to_csv(os.path.join(participant_dir, output_file_name))
+
+    if delete_video_after:
+        try:
+            os.remove(video_path)
+        except:
+            pass
